b880f664393a14093a51e16d8f070cb36febe7fc
Alexander Rubenov <alexander.rubenov@gmail.com>
2019-12-07 22:00:06 +0300

    added remove from cart

--- src/store/reducers/cart.js
+++ src/store/reducers/cart.js
@@ -5,12 +5,23 @@ export const cartReducer = (
   action
 ) => {
   switch (action.type) {
-    case 'ADD_TO_CART':
+    case 'ADD_TO_CART': {
       const {id} = action.payload
       return {
         ...cartState,
         [id]: cartState[id] ? cartState[id] + 1 : 1,
       }
+    }
+    case 'REMOVE_FROM_CART': {
+      const {id} = action.payload
+      if (!cartState[id]) {
+        return cartState
+      }
+      return {
+        ...cartState,
+        [id]: cartState[id] ? cartState[id] - 1 : 0,
+      }
+    }
   }
   return cartState
 }
